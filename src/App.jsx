402dae2f51819b6f0d3416d12cc4500cd9f2492b
Kosisochukwu Emmanuel <realitynuel@gmail.com>
2025-03-09 18:06:48 +0100

    finished working on the navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import Home from './pages/Home/Home'
+import Navbar from './components/Navbar/Navbar'
 
 const App = () => {
   return (
     <div>
-      <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+      <Navbar/>
+    {/* <Home/> */}
     </div>
   )
 }
