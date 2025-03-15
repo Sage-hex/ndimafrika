2abd1e1b7e40c522986804e817cfaf16205ce30d
Kosisochukwu Emmanuel <realitynuel@gmail.com>
2025-03-15 20:44:05 +0100

    Worked on a function to match special routes in the Layoutjs

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -21,7 +21,7 @@
 // export default Layout;
 
 
-import React from "react";
+import React, { useMemo } from "react";
 import { Outlet, useLocation } from "react-router-dom";
 import Navbar from "../components/Navbar/Navbar";
 import Footer from "../components/Footer/Footer";
@@ -32,15 +32,23 @@ function Layout() {
 
   const location = useLocation();
 
-  const isDashboard = location.pathname.startsWith('/userdashboard') || location.pathname.startsWith('/admin') ||  location.pathname.startsWith('/userLogin');
+  // const specialRoutes = ['/userdashboard','/admin','userLogin','/userSignup'];
+
+  const specialRoutes = useMemo(() => new Set(['/userdashboard','/admin','userLogin','/userSignup']),[]);
+  const isSpecialRoutes = specialRoutes.has(location.pathname);
+  // const isDashboard = location.pathname.inludes
+
+  // const isDashboard = location.pathname.startsWith('/userdashboard') || location.pathname.startsWith('/admin') ||  location.pathname.startsWith('/userLogin');
 
   return (
     <div className="flex flex-col  min-h-screen">
-    {!isDashboard && <Navbar />}
+    {/* {!isDashboard && <Navbar />} */}
+    {!specialRoutes && <Navbar/>}
       <main className="flex-grow ">
         <Outlet />
       </main>
-      {!isDashboard && <Footer/>}
+      {/* {!isDashboard && <Footer/>} */}
+      {!specialRoutes && <Footer/>}
     </div>
   );
 }
