8da1663ef36ffabc104cef131dbc5bd73848d2f1
Kosisochukwu Emmanuel <realitynuel@gmail.com>
2025-03-31 18:54:36 +0100

    Update Vite build output directory to 'build' for Render compatibility

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,15 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:'/',
-  resolve:{
-    alias:{
-      '@':'/src',
+  base: '/',
+  resolve: {
+    alias: {
+      '@': '/src',
     },
-
   },
-  plugins: [react(), tailwindcss(), ],
+  plugins: [react(), tailwindcss()],
   assetsInclude: ['**/*.JPG'],
+  build: {
+    outDir: 'build', // Add this line to change the output directory to 'build'
+  },
 });
